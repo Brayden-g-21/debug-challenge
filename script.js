c5d3c3f661e377042585227dcd08eac866fa4bd6
Brayden-g-21 <brayden.jgibson15@gmail.com>
2023-11-16 17:39:31 -0700

    fixed final issue

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
 
-let input = document.querySelector("#name");
-let display = document.querySelector(".display-text");
-
-input.addEventListener("input", function() {
-   const name = input.value;
-   
-   display.textContent = name;
-});
-
-function simpleMath(x, y) {
-    console.log(x + y)
-   };
-
-   simpleMath(2, 3);
