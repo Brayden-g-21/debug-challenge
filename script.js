2a111a49db89beefcca69c1647095a6b42f7e7f5
Brayden-g-21 <brayden.jgibson15@gmail.com>
2023-11-16 17:33:37 -0700

    completed js but having reference issue

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 
-let input = document.querySelector("name");
-let display = document.querySelector("display-text");
+let input = document.querySelector("#name");
+let display = document.querySelector(".display-text");
 
-input.addEventListener("input", function () {
+input.addEventListener("input", function() {
    const name = input.value;
    
    display.textContent = name;
-   
-   
-   
-   console.log(simpleMath(2, 3));
-})
-// missing function?
+});
 
-// you should see 5 in the output of your console
+function simpleMath(x, y) {
+    console.log(x + y)
+   };
+
+   simpleMath(2, 3);
